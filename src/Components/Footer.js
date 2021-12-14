c70092f8a23b867c1c45f87c61eacf14db82667b
abel-parada <abel.parada.millan@gmail.com>
2021-12-14 10:26:55 +0200

    Updated:footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = (props) => {
     return (
         <div className="footer">
         <footer>
-            <p>Abel Parada. &copy; 2021</p>
+             <p>Abel Parada. &copy; 2021. Visit my <strong><a href="https://github.com/abel-parada">Github Account</a></strong></p>
         </footer>
     </div>
     );
